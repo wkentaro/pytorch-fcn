f862588db6d215b95c7503162f8f7918ad1d9977
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-02-19 03:35:40 +0900

    Create examples dir with setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='torchfcn',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=open('requirements.txt').readlines(),
+    author='Kentaro Wada',
+    author_email='www.kentaro.wada@gmail.com',
+    license='MIT',
+    url='https://github.com/wkentaro/torchfcn',
+)

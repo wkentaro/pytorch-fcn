ea5dcd7691b2e654395741be7d9598b838ebfc45
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-02-26 00:47:51 +0900

    Include ext

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     name='torchfcn',
     version='0.2',
     packages=find_packages(),
+    package_data={'torchfcn': ['torchfcn/ext/*']},
+    include_package_data=True,
     author='Kentaro Wada',
     author_email='www.kentaro.wada@gmail.com',
     license='MIT',

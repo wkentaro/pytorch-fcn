72803d5f6ed16f891637d19f32c4f1d673f8d963
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-03-24 12:55:16 +0900

    1.0

--- setup.py
+++ setup.py
@@ -4,14 +4,26 @@ from setuptools import find_packages
 from setuptools import setup
 
 
+__version__ = '1.0'
+
+
 setup(
     name='torchfcn',
-    version='0.2',
+    version=__version__,
     packages=find_packages(),
+    description='PyTorch Implementation of Fully Convolutional Networks.',
+    long_description=open('README.md').read(),
     package_data={'torchfcn': ['torchfcn/ext/*']},
     include_package_data=True,
     author='Kentaro Wada',
     author_email='www.kentaro.wada@gmail.com',
     license='MIT',
     url='https://github.com/wkentaro/torchfcn',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
 )

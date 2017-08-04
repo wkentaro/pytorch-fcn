96f530dfa0d7dbea57a5c23e7509e648966ba287
Bicheng Gao <volz.kz.g@gmail.com>
2017-08-04 23:09:47 +0000

    use aria2c to increase stability

--- examples/voc/download_dataset.sh
+++ examples/voc/download_dataset.sh
@@ -6,18 +6,18 @@ mkdir -p $DIR
 cd $DIR
 
 if [ ! -e benchmark_RELEASE ]; then
-  wget http://www.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/semantic_contours/benchmark.tgz -O benchmark.tar
+  aria2c -c http://www.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/semantic_contours/benchmark.tgz -O benchmark.tar
   tar -xvf benchmark.tar
 fi
 
 if [ ! -e VOCdevkit/VOC2011 ]; then
-  wget http://host.robots.ox.ac.uk/pascal/VOC/voc2011/VOCtrainval_25-May-2011.tar
+  aria2c -c http://host.robots.ox.ac.uk/pascal/VOC/voc2011/VOCtrainval_25-May-2011.tar
   tar -xvf VOCtrainval_25-May-2011.tar
   mv TrainVal/VOCdevkit/VOC2011 VOCdevkit/VOC2011
   rm -rf TrainVal
 fi
 
 if [ ! -e VOCdevkit/VOC2012 ]; then
-  wget http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+  aria2c -c http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
   tar -xvf VOCtrainval_11-May-2012.tar
 fi

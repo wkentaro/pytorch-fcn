01b5e0fff7e4b5db56761673cb2925f09be98391
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-08-08 09:11:58 +0900

    Copy FCN16s from caffe

--- examples/voc/model_caffe_to_torch.py
+++ examples/voc/model_caffe_to_torch.py
@@ -4,33 +4,37 @@ import os.path as osp
 import pkg_resources
 import sys
 
-import caffe
 import torch
+import caffe  # NOQA: must be after import caffe
 import torchfcn
 
 
-print('==> Loading FCN32s model from Caffe')
-pkg_root = pkg_resources.get_distribution('torchfcn').location
-sys.path.insert(0, osp.join(pkg_root, 'torchfcn/ext/fcn.berkeleyvision.org'))
-caffe_prototxt = osp.join(
-    pkg_root, 'torchfcn/ext/fcn.berkeleyvision.org/voc-fcn32s/deploy.prototxt')
-caffe_model_path = osp.expanduser(
-    '~/data/models/caffe/fcn32s-heavy-pascal.caffemodel')
-caffe_model = caffe.Net(caffe_prototxt, caffe_model_path, caffe.TEST)
+# for size_reactive in [32, 16, 8]:
+for size_reactive in [32, 16]:
+    print('==> Loading caffe model of FCN%ds' % size_reactive)
+    pkg_root = pkg_resources.get_distribution('torchfcn').location
+    sys.path.insert(0, osp.join(pkg_root, 'torchfcn/ext/fcn.berkeleyvision.org'))
+    caffe_prototxt = osp.join(
+        pkg_root,
+        'torchfcn/ext/fcn.berkeleyvision.org/voc-fcn%ds/deploy.prototxt' %
+        size_reactive)
+    caffe_model_path = osp.expanduser(
+        '~/data/models/caffe/fcn%ss-heavy-pascal.caffemodel' % size_reactive)
+    caffe_model = caffe.Net(caffe_prototxt, caffe_model_path, caffe.TEST)
 
-torch_model = torchfcn.models.FCN32s()
+    torch_model = getattr(torchfcn.models, 'FCN%ds' % size_reactive)()
 
-torch_model_params = torch_model.parameters()
-for name, p1 in caffe_model.params.iteritems():
-    p2 = torch_model_params.next()
-    print('%s: %s -> %s' % (name, p1[0].data.shape, p2.data.size()))
-    p2.data = torch.from_numpy(p1[0].data)
-    if len(p1) == 2:
+    torch_model_params = torch_model.parameters()
+    for name, p1 in caffe_model.params.iteritems():
         p2 = torch_model_params.next()
-        print('%s: %s -> %s' % (name, p1[1].data.shape, p2.data.size()))
-        p2.data = torch.from_numpy(p1[1].data)
+        print('%s: %s -> %s' % (name, p1[0].data.shape, p2.data.size()))
+        p2.data = torch.from_numpy(p1[0].data)
+        if len(p1) == 2:
+            p2 = torch_model_params.next()
+            print('%s: %s -> %s' % (name, p1[1].data.shape, p2.data.size()))
+            p2.data = torch.from_numpy(p1[1].data)
 
-torch_model_path = osp.expanduser(
-    '~/data/models/pytorch/fcn32s-heavy-pascal.pth')
-print('==> Saving FCN32s PyTorch model to: %s' % torch_model_path)
-torch.save(torch_model.state_dict(), torch_model_path)
+    torch_model_path = osp.expanduser(
+        '~/data/models/pytorch/fcn%ss-heavy-pascal.pth' % size_reactive)
+    torch.save(torch_model.state_dict(), torch_model_path)
+    print('==> Saved pytorch model: %s' % torch_model_path)

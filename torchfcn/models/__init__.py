0845e8aceacf7889a69304140412215e11f58a3e
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-08-08 11:35:41 +0900

    Add FCN8sAtOnce model

--- torchfcn/models/__init__.py
+++ torchfcn/models/__init__.py
@@ -2,4 +2,5 @@
 from .fcn32s import FCN32s
 from .fcn16s import FCN16s
 from .fcn8s import FCN8s
+from .fcn8s import FCN8sAtOnce
 from .vgg import VGG16
--- torchfcn/models/fcn8s.py
+++ torchfcn/models/fcn8s.py
@@ -1,9 +1,14 @@
+import os.path as osp
+
 import torch.nn as nn
 
 from .fcn32s import get_upsampling_weight
 
 
 class FCN8s(nn.Module):
+
+    pretrained_model = \
+        osp.expanduser('~/data/models/pytorch/fcn8s-heavy-pascal.pth')
 
     def __init__(self, n_class=21):
         super(FCN8s, self).__init__()
@@ -153,3 +158,100 @@ class FCN8s(nn.Module):
             if l1.bias is not None:
                 assert l1.bias.size() == l2.bias.size()
                 l2.bias.data.copy_(l1.bias.data)
+
+
+class FCN8sAtOnce(FCN8s):
+
+    pretrained_model = \
+        osp.expanduser('~/data/models/pytorch/fcn8s-atonce-heavy-pascal.pth')
+
+    def forward(self, x):
+        h = x
+        h = self.relu1_1(self.conv1_1(h))
+        h = self.relu1_2(self.conv1_2(h))
+        h = self.pool1(h)
+
+        h = self.relu2_1(self.conv2_1(h))
+        h = self.relu2_2(self.conv2_2(h))
+        h = self.pool2(h)
+
+        h = self.relu3_1(self.conv3_1(h))
+        h = self.relu3_2(self.conv3_2(h))
+        h = self.relu3_3(self.conv3_3(h))
+        h = self.pool3(h)
+        pool3 = h  # 1/8
+
+        h = self.relu4_1(self.conv4_1(h))
+        h = self.relu4_2(self.conv4_2(h))
+        h = self.relu4_3(self.conv4_3(h))
+        h = self.pool4(h)
+        pool4 = h  # 1/16
+
+        h = self.relu5_1(self.conv5_1(h))
+        h = self.relu5_2(self.conv5_2(h))
+        h = self.relu5_3(self.conv5_3(h))
+        h = self.pool5(h)
+
+        h = self.relu6(self.fc6(h))
+        h = self.drop6(h)
+
+        h = self.relu7(self.fc7(h))
+        h = self.drop7(h)
+
+        h = self.score_fr(h)
+        h = self.upscore2(h)
+        upscore2 = h  # 1/16
+
+        h = self.score_pool4(pool4 * 0.01)  # XXX: scaling to train at once
+        h = h[:, :, 5:5 + upscore2.size()[2], 5:5 + upscore2.size()[3]]
+        score_pool4c = h  # 1/16
+
+        h = upscore2 + score_pool4c  # 1/16
+        h = self.upscore_pool4(h)
+        upscore_pool4 = h  # 1/8
+
+        h = self.score_pool3(pool3 * 0.0001)  # XXX: scaling to train at once
+        h = h[:, :,
+              9:9 + upscore_pool4.size()[2],
+              9:9 + upscore_pool4.size()[3]]
+        score_pool3c = h  # 1/8
+
+        h = upscore_pool4 + score_pool3c  # 1/8
+
+        h = self.upscore8(h)
+        h = h[:, :, 31:31 + x.size()[2], 31:31 + x.size()[3]].contiguous()
+
+        return h
+
+    def copy_params_from_vgg16(self, vgg16):
+        features = [
+            self.conv1_1, self.relu1_1,
+            self.conv1_2, self.relu1_2,
+            self.pool1,
+            self.conv2_1, self.relu2_1,
+            self.conv2_2, self.relu2_2,
+            self.pool2,
+            self.conv3_1, self.relu3_1,
+            self.conv3_2, self.relu3_2,
+            self.conv3_3, self.relu3_3,
+            self.pool3,
+            self.conv4_1, self.relu4_1,
+            self.conv4_2, self.relu4_2,
+            self.conv4_3, self.relu4_3,
+            self.pool4,
+            self.conv5_1, self.relu5_1,
+            self.conv5_2, self.relu5_2,
+            self.conv5_3, self.relu5_3,
+            self.pool5,
+        ]
+        for l1, l2 in zip(vgg16.features, features):
+            if isinstance(l1, nn.Conv2d) and isinstance(l2, nn.Conv2d):
+                assert l1.weight.size() == l2.weight.size()
+                assert l1.bias.size() == l2.bias.size()
+                l2.weight.data.copy_(l1.weight.data)
+                l2.bias.data.copy_(l1.bias.data)
+        for i, name in zip([0, 3], ['fc6', 'fc7']):
+            l1 = vgg16.classifier[i]
+            l2 = getattr(self, name)
+            l2.weight.data.copy_(l1.weight.data.view(l2.weight.size()))
+            l2.bias.data.copy_(l1.bias.data.view(l2.bias.size()))

658d58236bb3890c814beb5daa282158a3096309
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-08-08 09:27:10 +0900

    Fix evaluate.py

--- examples/voc/evaluate.py
+++ examples/voc/evaluate.py
@@ -29,7 +29,12 @@ def main():
     n_class = len(val_loader.dataset.class_names)
 
     print('==> Loading model file: %s' % model_file)
-    model = torchfcn.models.FCN32s(n_class=21)
+    if osp.basename(model_file).startswith('fcn32s'):
+        model = torchfcn.models.FCN32s(n_class=21)
+    elif osp.basename(model_file).startswith('fcn16s'):
+        model = torchfcn.models.FCN16s(n_class=21)
+    else:
+        raise ValueError
     if torch.cuda.is_available():
         model = model.cuda()
     model_data = torch.load(model_file)
@@ -51,7 +56,7 @@ def main():
         score = model(data)
 
         imgs = data.data.cpu()
-        lbl_pred = score.data.max(1)[1].cpu().numpy()[:, 0, :, :]
+        lbl_pred = score.data.max(1)[1].cpu().numpy()[:, :, :]
         lbl_true = target.data.cpu()
         for img, lt, lp in zip(imgs, lbl_true, lbl_pred):
             img, lt = val_loader.dataset.untransform(img, lt)
@@ -59,7 +64,7 @@ def main():
             label_preds.append(lp)
             if len(visualizations) < 9:
                 viz = fcn.utils.visualize_segmentation(
-                    lp, lt, img, n_class=n_class)
+                    lbl_pred=lp, lbl_true=lt, img=img, n_class=n_class)
                 visualizations.append(viz)
     metrics = torchfcn.utils.label_accuracy_score(
         label_trues, label_preds, n_class=n_class)

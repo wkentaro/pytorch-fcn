6aeed6bab2f2903a788f0f4df886ed7292c9a05c
Matt Barnes <barnes.matt.j@gmail.com>
2018-01-03 08:07:38 -0800

    Update for PyTorch >= 0.3

--- torchfcn/trainer.py
+++ torchfcn/trainer.py
@@ -20,9 +20,9 @@ def cross_entropy2d(input, target, weight=None, size_average=True):
     # input: (n, c, h, w), target: (n, h, w)
     n, c, h, w = input.size()
     # log_p: (n, c, h, w)
-    log_p = F.log_softmax(input)
+    log_p = F.log_softmax(input, dim=1)
     # log_p: (n*h*w, c)
-    log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
+    log_p = log_p.transpose(1, 2).transpose(2, 3).contiguous()
     log_p = log_p[target.view(n, h, w, 1).repeat(1, 1, 1, c) >= 0]
     log_p = log_p.view(-1, c)
     # target: (n*h*w,)
